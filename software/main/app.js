df461da4e772bacb60be86292e2f87485ccd5985
JPSKE15 <hokiz_1999@hotmail.com>
2018-07-22 04:47:58 +0700

    ...

--- software/main/app.js
+++ software/main/app.js
@@ -3,6 +3,7 @@ let cur_pos = '0'
 let watering_min = '0'
 let ready = '0'
 let lux = '0'
+let luxs = 0
 let humit = '0'
 let temp = '0'
 let cur_per = '0'
@@ -24,9 +25,9 @@ let time_p4 = '0'
 let time_p5 = '0'
 let time_p6 = '0'
 let pos_0 = '0'
-let pos_1 = '1'
-let pos_2 = '1'
-let pos_3 = '1'
+let pos_1 = '0'
+let pos_2 = '0'
+let pos_3 = '0'
 let emergency_button = '0'
 
 //now
@@ -58,19 +59,26 @@ let check_d5 = '0'
 let check_d6 = '0'
 let dawn = '0'
 let dusk = '0'
-
+let number = 0
+const check_lux = '0'
 $(function () {
+    $(set_position())
     setInterval(function () {
         //get time
         let date = new Date()
         date_hr = date.getHours()
         date_min = date.getMinutes()
+        
+        console.log(`1: ${pos_1}`)
+        console.log(`2: ${pos_2}`)
+        console.log(`3: ${pos_3}`)
 
         //get data from hardware
         $(get_value)
-        cur_per = parseInt(cur_per)
+        snumber = number.toString()
+        cur_per = snumber
         if(date_hr == hr && date_min == min){
-            $(set(moving))
+            $(move())
         }
 
         if(cur_per === '0'){
@@ -78,6 +86,7 @@ $(function () {
             document.getElementById("pos-0").style.backgroundRepeat = "no-repeat";
             document.getElementById("pos-0").style.backgroundPosition = "center center";
         }else{
+            document.getElementById("pos-0").style.backgroundImage = "";
             document.getElementById("pos-0").style.backgroundColor = "rgba(66, 66, 66, 0.70)";
         }
         if(cur_per === '1'){
@@ -92,6 +101,7 @@ $(function () {
                 document.getElementById("plant-s2").style.border = "5px solid blue";
             }
         } else {
+            document.getElementById("pos-1").style.backgroundImage = "";
             document.getElementById("pos-1").style.backgroundColor = "rgba(66, 66, 66, 0.70)";
             document.getElementById("plant-s1").style.border = "5px solid black";
             document.getElementById("plant-s2").style.border = "5px solid black";
@@ -109,6 +119,7 @@ $(function () {
                 document.getElementById("plant-s4").style.border = "5px solid blue";
             }
         }else{
+            document.getElementById("pos-2").style.backgroundImage = "";
             document.getElementById("pos-2").style.backgroundColor = "rgba(66, 66, 66, 0.70)";
             document.getElementById("plant-s3").style.border = "5px solid black";
             document.getElementById("plant-s4").style.border = "5px solid black";
@@ -126,12 +137,17 @@ $(function () {
                 document.getElementById("plant-s6").style.border = "5px solid blue";
             }
         } else {
+            document.getElementById("pos-3").style.backgroundImage = "";
             document.getElementById("pos-3").style.backgroundColor = "rgba(66, 66, 66, 0.70)";
             document.getElementById("plant-s5").style.border = "5px solid black";
             document.getElementById("plant-s6").style.border = "5px solid black";
         }
 
-        
+        if(dawn === '1'){
+            if(luxs > check_lux){
+                $(move())
+            }
+        }
 
         if (change === '1') {
             change = '0'
@@ -141,9 +157,10 @@ $(function () {
     }, 1000)
     
     //emergency butt
-    // $('#emergency-button').on('click', function () {
+    $('#emergency-button').on('click', function () {
     //    $(set('emer','1'))
-    // })
+        number++;
+    })
 
     $('#hr0').click(function () {
         console.log('hr0')
@@ -152,15 +169,18 @@ $(function () {
     //submit[now]
     $('#submit-n').on('click', function () {
         $(select_plantn('','','1'))
+        $(set_position())
         $(move('3'))
     })
     //submit[time]
     $('#submit-t').on('click', function () {
         $(select_plantt('','','1'))
+        $(set_position())
     })
     //submit[day]
     $('#submit-d').on('click', function () {
         $(select_plantd('','','1'))
+        $(set_position())
     })
 
      $(`#plant-n1`).click(function () {
@@ -379,6 +399,15 @@ let GET = (u) => {
 
 
 let get_value = () => {
+    let t = GET('pos_1').then((res) => {
+        pos_1 = res
+    })
+    let tt = GET('pos_2').then((res) => {
+        pos_2 = res
+    })
+    let ttt = GET('pos_3').then((res) => {
+        pos_3 = res
+    })
     let test = GET('cur_pos').then((res) => {
         cur_pos = res
     })
@@ -390,6 +419,7 @@ let get_value = () => {
     })
     let test4 = GET('lux').then((res) => {
         lux = res
+        luxs = parseInt(res)
         $('#bright').html(`${lux} lx`)
     })
     let test5 = GET('humit').then((res) => {
@@ -414,50 +444,63 @@ let set = (u, send) => {
 }
 
 let next_pos = (now) => {
-    if (now == 1) {
-        if (pos_2 == 1) {
-            return 2
-        } else if (pos_3 == 1) {
-            return 3
-        } else {
-            return 0
+    if ( now == 0 ){
+        if( pos_1 == 1){
+            return '1'
+        }else if ( pos_2 ==1 ){
+            return '2'
+        }else if ( pos_3 == 1){
+            return '3'
+        }else {
+            return '0'
         }
-    } else if (pos_2 == 1) {
-        if (pos_3 == 1) {
-            return 3
+    }else if (now == 1) {
+        if (pos_2 == 1) {
+            return '2'
+        } else if (pos_3 == 1) {
+            return '3'
         } else {
-            return 0
+            return '0'
+        }
+    } else if (now == 2) {
+        if (pos_3 == 1) {
+            return '3'
+        } else {
+            return '0'
         }
     } else {
-        return 0
+        return '0'
     }
 }
 
 let move = (position) => {
-    // let last_data = 0
+    let last_data = '0'
+    let work_done = '0'
     // while (cur_per <= position) {
-    //     $(get_value)
-    //     if (moving === '0' && cur_per !== '0' && done === '0') {
-    //         // $(set('time-watering', ''))
+    let loop = setInterval(function() {
+        console.log('loop')
+        $(get_value)
+        if (watering === '0' && cur_per !== '0' && done === '0') {
+            // $(set('watering', '60'))
 
-    //         done = '1'
-    //     }
-    //     else if (watering !== last_data && last_data !== 0) {
-    //         let next_position = $(next_pos(cur_per))
-    //         cur_per =next_position
-    //         // $(set('move', next_position))
-    //     }
-    //     else if (moving === '1') {
-    //         done = '0'
-    //     }
-    //     last_data = watering
-    // }
-    let new_watering = '0'
-    let word_done = '0'
-    let loop = setInterval(function(){
-        let next_position = $(next_pos(cur_per))
-                cur_per =next_position
-    },10000)
+            done = '1'
+        }
+        else if ((watering !== last_data && last_data !== '0')|| cur_per === '0') {
+            let next_position = $(next_pos(cur_per))
+            // $(set('move', next_position))
+            // cur_per =next_position
+        }
+        else if (moving === '1') {
+            done = '0'
+        }
+        if(cur_per == position) {
+            work_done = '1'
+        }
+        if (cur_per == '0' && work_done == '1'){
+            clearInterval(loop);
+        }
+        last_data = watering
+    },1000) 
 }
 
 let select_plantn = (plant,select,del) => {
@@ -510,6 +553,14 @@ let select_plantn = (plant,select,del) => {
                 check_p6 = '0'
             }
         }else if(del === '1'){
+            if(check_p1 == '1' || check_p2 == '1'){
+                pos_1 = '1'
+            }if(check_p3 == '1' || check_p4 == '1'){
+                pos_2 = '1'
+            }
+            if(check_p5 == '1' || check_p6 == '1'){
+                pos_3 = '1'
+            }
             check_p1='0'
             check_p2='0'
             check_p3='0'
@@ -575,6 +626,14 @@ let select_plantt = (plant, select,del) => {
                 check_t6 = '0'
             }
         }else if(del === '1'){
+            if(check_t1 == '1' || check_t2 == '1'){
+                pos_1 = '1'
+            }if(check_t3 == '1' || check_t4 == '1'){
+                pos_2 = '1'
+            }
+            if(check_t5 == '1' || check_t6 == '1'){
+                pos_3 = '1'
+            }
             check_t1='0'
             check_t2='0'
             check_t3='0'
@@ -641,6 +700,14 @@ let select_plantd = (plant, select,del) => {
                 check_d6 = '0'
             }
         }else if(del === '1'){
+            if(check_d1 == '1' || check_d2 == '1'){
+                pos_1 = '1'
+            }if(check_d3 == '1' || check_d4 == '1'){
+                pos_2 = '1'
+            }
+            if(check_d5 == '1' || check_d6 == '1'){
+                pos_3 = '1'
+            }
             check_d1='0'
             check_d2='0'
             check_d3='0'
@@ -655,4 +722,9 @@ let select_plantd = (plant, select,del) => {
             document.getElementById(`plant-d6`).style.border = "5px solid black";
             
         }
+}
+let set_position = () => {
+    $(set('pos_1',pos_1))
+    $(set('pos_2',pos_2))
+    $(set('pos_3',pos_3))
 }
